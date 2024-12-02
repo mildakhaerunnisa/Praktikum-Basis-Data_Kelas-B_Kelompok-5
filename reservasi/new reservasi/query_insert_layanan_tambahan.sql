ab39db7fc3e1443fffd93489d0a20c266c055601
mildakhaerunnisa <146157113+mildakhaerunnisa@users.noreply.github.com>
2024-12-03 00:25:27 +0700

    Add files via upload

---
+++ reservasi/new reservasi/query_insert_layanan_tambahan.sql
@@ -0,0 +1,13 @@
+INSERT INTO Layanan_Tambahan(nama_layanan, harga, deskripsi)
+VALUES ('Spa', 150000.00, 'Paket spa selama 1 jam'),
+('Laundry', 15000.00, 'Layanan cuci pakaian dan setrika per kilogram'),
+('Room Service', 80000.00, 'Pesan makanan/minuman di kamar'),
+('Extra Bed', 100000.00, 'Penambahan tempat tidur di kamar'),
+('Karaoke', 140000.00,'Sewa ruang karaoke per jam'),
+('Paket Honeymoon', 500000.00, 'Layanan spesial untuk pasangan pengantin baru'),
+('Paket Anniversary', 400000.00, 'Dekorasi kamar khusus untuk perayaan ulang tahun'),
+('Paket Romantic Dinner', 300000.00,'Makan malam yang romantis untuk pasangan'),
+('Paket Fitness', 60000.00, 'Layanan akses ke gym per hari'),
+('Antar-jemput Bandara', 700000.00, 'Layanan antar-jemput dari/ke bandara'),
+('Penyewaan Ruang Meeting', 500000.00, 'Sewa ruang meeting lengkap dengan fasilitas'),
+('Extra Sofa', 100000.00, 'Penambahan sofa di kamar')
---
+++ reservasi/new reservasi/query_insert_reservasi.sql
@@ -0,0 +1,28 @@
+INSERT INTO Reservasi (tanggal_check_in, tanggal_check_out, jumlah_tamu, status_reservasi, total_harga, catatan, id_tamu, id_kamar)
+VALUES ('2024-03-05', '2024-03-10', 2, 'confirmed', 4500000.00, 'Tolong kamar diperiksa untuk keamanan sebelum check-in oleh tamu VIP', 1, 41), --41 penthouse
+('2024-06-15','2024-06-20',2, 'confirmed', 4500000.00 , 'Mohon persiapkan ruangan dengan akses mudah ke jalur darurat dan tanpa akses publik langsung', 2, 42 ), --42 penthouse
+('2024-08-10','2024-08-12',2, 'checked_in', 2250000.00,'-',3,2), --double
+('2024-01-22', '2024-01-30', 1, 'confirmed', 4500000.00, 'Mohon persiapkan kamar bebas asap rokok', 4, 6), --single
+('2024-02-18','2024-02-19',3, 'checked_in', 10000000.00,'-',5,18), --presidential
+('2024-05-27','2024-05-29',2, 'confirmed', 9000000.00, 'Tolong persiapkan kamar dengan dekorasi romantis', 6,22), -- honeymoon
+('2024-09-12','2024-09-13',2, 'checked_in', 10000000.00,'-',7,26), --26 presidential
+('2024-07-08','2024-07-10',2, 'checked_in', 9000000.00,'-',8,27), --27 luxury
+('2024-11-02','2024-11-05',2, 'checked_in', 9000000.00,'-',9,43), --43 penthouse
+('2024-04-25','2024-04-30',2, 'checked_in', 12000000.00, '-', 10, 38), -- 38 skyview
+('2024-02-10','2024-02-12',3, 'checked_in', 13500000.00,'-',11,46), --46 penthouse
+('2024-12-20','2024-12-22', 3, 'checked_in', 4200000.00,'-',12,10), --10 family
+('2024-01-30','2024-02-01', 2, 'checked_in',4800000.00,'-',13,14), --14 junior
+('2024-10-19','2024-10-20', 2, 'checked_in', 4000000.00,'-',14,32), --32 business
+('2024-03-14','2024-03-16',2, 'confirmed', 9000000.00,'Minta kamar dengan sofa tambahan',15,34), --34 luxury
+('2024-09-03','2024-09-05',2, 'checked_in', 6000000.00, '-', 16,35), --35 executive
+('2024-07-22','2024-07-23',2, 'checked_in', 2400000.00, '-',17,30), -- 30 king
+('2024-04-05','2024-04-07',2, 'checked_in', 6000000.00, '-',18,33), -- 33 skyview
+('2024-06-30','2024-07-02',4, 'confirmed', 15000000.00, 'Tolong sediakan tempat tidur tambahan dan area bermain untuk anak-anak', 19,39), --39 presidential
+('2024-08-16','2024-08-18',3, 'checked_in', 13500000.00, '-',20,47), --47 penthouse
+('2024-05-11','2024-05-15',4, 'checked_in', 22500000.00, '-', 21,46), -- 46 penthouse
+('2024-01-18','2024-01-21',3, 'checked_out', 3200000.00,'Tempatnya sejuk, strategis, cabinnya bersih dan terawat', 22, 3), --3 cabin
+('2024-02-25','2024-02-26',1,'checked_out', 1200000.00, 'Kamar yang nyaman dan cukup untuk kunjungan yang singkat', 23, 7), --7 compact
+('2024-06-22','2024-06-24',2, 'checked_out', 6000000.00, 'Kamar yang sangat bagus untuk business trip, saya menyukai pemandangan balkonnya', 24,11), --11 executive
+('2024-08-02','2024-08-05',2, 'checked_out',8000000.00, 'Kamar yang bagus, ruang kerja yang sangat baik, dan layout ruangan indah dilihat', 25,15), --15 executive
+('2024-09-29','2024-10-03',2, 'checked_out', 11000000.00,'Suasana tenang dan damai, kamar dengan pemandangan laut yang indah',26,19), --19 cabana
+('2024-12-04','2024-12-06',2, 'checked_out', 6000000.00, 'Kamar bisnis yang sangat cocok untuk bekerja hingga larut malam, dengan pencahayaan yang sangat baik',27,23) --23 business 
